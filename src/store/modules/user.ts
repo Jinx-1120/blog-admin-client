56a15107b9d2fc231a1cabc83010b23d2325f753
jinhaidi <haidi.jin@baodanyun-inc.com>
2019-01-22 18:00:49 +0800

    add user store

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -5,22 +5,34 @@ Vue.use(Vuex)
 
 interface IState {
   login: boolean
-  userInfo: object
+  userInfo: object,
+  token: string
 }
 
 const state: IState = {
   login: false,
-  userInfo: {}
+  userInfo: {},
+  token: ''
 }
 const mutations: MutationTree<IState> = {
   'TOGGLE_LOGOUT' (state: IState, flag: boolean): void {
     state.login = flag
+  },
+  'SET_TOKEN' (state: IState, token: string): void {
+    state.token = token
+    window.localStorage.setItem('TOKEN', token)
   }
 }
 const actions: ActionTree<IState, any> = {
   async login ({ commit }, user): Promise<Ajax.AjaxResponse> {
     const res: Ajax.AjaxResponse = await httpservice.login({ ...user })
-    commit('TOGGLE_LOGOUT', true)
+    if (res.code === 200) {
+      commit('TOGGLE_LOGOUT', true)
+      commit('SET_TOKEN', res.data.token)
+      const info: Ajax.AjaxResponse = await httpservice.getInfo()
+    } else {
+      commit('TOGGLE_LOGOUT', false)
+    }
     return res
   },
   async logout ({ commit }): Promise<Ajax.AjaxResponse> {
